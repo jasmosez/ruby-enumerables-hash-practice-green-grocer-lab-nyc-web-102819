82efb433014439aa9110a4259892d731f29b570d
James Schaffer <jas.schaffer@gmail.com>
2019-09-18 06:34:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,9 +28,9 @@ puts "coupons"
 puts coupons
  
   # transform coupons array to be array of hashes similar in structure to cart hash
- coupons_array = coupons.map { |element|
+ coupons_array = coupons.reduce({}) { |new_hash, element|
    
-      element[:item] => {
+      new_hash = element[:item] => {
         :num => element[:num], 
         :cost => element[:cost]
       }
