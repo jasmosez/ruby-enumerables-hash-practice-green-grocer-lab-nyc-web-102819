70399ca49eb297919b6844e7e7b3f791b9109528
James Schaffer <jas.schaffer@gmail.com>
2019-09-18 03:19:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
   # consolidate coupons_as_hash in case there are multiple coupons for the same item
   
     #step through each item in cart array and map it to a new hash
-    coupons_as_hash = coupons_as_hash.reduce({}) { |new_hash, coupon_item_hash|
+    coupons_reduced_hash = coupons_as_hash.reduce({}) { |new_hash, coupon_item_hash|
       
       # create an array of the keys of the hash that is an element of cart
       coupon_item_keys = coupon_item_hash.keys
@@ -72,17 +72,17 @@ def apply_coupons(cart, coupons)
   
     # if the item with a coupon is in our cart
     # and if our cart count of that item is enough to make use of coupon
-    if cart[item_having_coupon] && cart[item_having_coupon][:count] >= coupons_as_hash[item_having_coupon][:num]
+    if cart[item_having_coupon] && cart[item_having_coupon][:count] >= coupons_reduced_hash[item_having_coupon][:num]
     
       # apply each coupon such that it our cart hash    
       # 1. create new item in cart for item with coupon with appropriate price, clearance and count
       cart["#{item_having_coupon} W/COUPON"] = {
-        :price => (coupons_as_hash[item_having_coupon][:cost]/coupons_as_hash[item_having_coupon][:num]),
+        :price => (coupons_reduced_hash[item_having_coupon][:cost]/coupons_reduced_hash[item_having_coupon][:num]),
         :clearance => cart[item_having_coupon][:clearance],
-        :count => coupons_as_hash[item_having_coupon][:num]
+        :count => coupons_reduced_hash[item_having_coupon][:num]
       }
       # 2. reduce count for original item
-      cart[item_having_coupon][:count] -= coupons_as_hash[item_having_coupon][:num]
+      cart[item_having_coupon][:count] -= coupons_reduced_hash[item_having_coupon][:num]
     end
   
   }
